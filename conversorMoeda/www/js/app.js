28e5cd994660c7bba22722f4eb1c07af5474aedb
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-07-14 22:43:26 -0300

    criação

--- conversorMoeda/www/js/app.js
+++ conversorMoeda/www/js/app.js
@@ -1,4 +1,34 @@
-fetch ('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-.then(response => response.json())
+function update() {
+  let moeda1 = $('#cotacaoDe :selected').val();
+
+  document.getElementById('value').value = moeda1.value;
+}
+
+update();
 
 
+function obterValor() {
+  let input = document.querySelector('#valor').value;
+
+  $.ajax({
+    type: "GET",
+    url: `https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`,
+    success: (function (cotacao) {
+      let valor = cotacao.high
+      let codigo = cotacao.code
+      let data = cotacao.create_data
+
+      document.getElementById('resposta').innerHTML =
+        set ` 
+        
+      Calculando...
+          
+          `
+
+    })
+  })
+}
+
+
+
+document.querySelector('#btn-calcular').addEventListener('click', obterValor)
