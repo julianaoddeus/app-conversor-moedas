359562019cc2d63a31598c901753664f3509eea3
julianaoddeus <64862169+julianaoddeus@users.noreply.github.com>
2022-07-18 20:13:14 -0300

    up finalizado

--- conversorMoeda/www/js/app.js
+++ conversorMoeda/www/js/app.js
@@ -6,52 +6,75 @@ const valorInputAtualizado = document.querySelector('#input-valor')
 
 let mudancaValorInput = {}
 
-const apiURL = `https://v6.exchangerate-api.com/v6/0b15ef5e4192c33c16d03bea/latest/USD`
+const getURL = moeda =>
+  `https://v6.exchangerate-api.com/v6/0b15ef5e4192c33c16d03bea/latest/${moeda}`
 
 //obtem um array de propriedades das moedas
-const fetchBuscaTaxaCambio = async () => {
+const fetchBuscaTaxaCambio = async apiURL => {
   const response = await fetch(apiURL)
   return await response.json()
 }
 
-
 //mapeando o array e colocando nas options e fixando em USD e BRL
 const init = async () => {
-  const obterDados = await fetchBuscaTaxaCambio()
+  const obterDados = await fetchBuscaTaxaCambio(getURL('USD'))
 
   mudancaValorInput = {
     ...obterDados
   } // disponibiliza para toda aplicação os dados obtidos no request
 
-  const getOptions = selectedMoeda => Object.keys(obterDados.conversion_rates)
-    .map(moeda => `<option ${moeda === selectedMoeda ? 'selected' : '' }>${moeda}</option>`).join('')
+  const getOptions = selectedMoeda =>
+    Object.keys(mudancaValorInput.conversion_rates)
+      .map(
+        moeda =>
+          `<option ${
+            moeda === selectedMoeda ? 'selected' : ''
+          }>${moeda}</option>`
+      )
+      .join('')
 
   primeiraMoeda.innerHTML = getOptions('USD')
   segundaMoeda.innerHTML = getOptions('BRL')
 
-
-  converterValor.textContent = `R$ ${obterDados.conversion_rates.BRL.toFixed(2)}`
-  moedaConvertida.textContent = `1 USD ${ obterDados.conversion_rates.BRL } BRL`
-
-
+  converterValor.textContent = `R$ ${mudancaValorInput.conversion_rates.BRL.toFixed(
+    2
+  )}`
+  moedaConvertida.textContent = `1 USD ${mudancaValorInput.conversion_rates.BRL} BRL`
 }
 
 //pegando o valor do input e multiplicando os valores
 valorInputAtualizado.addEventListener('input', event => {
-  converterValor.textContent = (event.target.value * mudancaValorInput.conversion_rates[segundaMoeda.value]).toFixed(2)
+  converterValor.textContent = (
+    event.target.value * mudancaValorInput.conversion_rates[segundaMoeda.value]
+  ).toFixed(2)
+
 
 })
-
 
 segundaMoeda.addEventListener('input', event => {
-  const moedaValorAtualizado = mudancaValorInput.conversion_rates[event.target.value].toFixed(2)
+  const moedaValorAtualizado =
+    mudancaValorInput.conversion_rates[event.target.value].toFixed(2)
   console.log(moedaValorAtualizado)
 
-  converterValor.textContent = (valorInputAtualizado.value * moedaValorAtualizado).toFixed(2)
-  moedaConvertida.textContent = `1 USD = ${1 * mudancaValorInput.conversion_rates[segundaMoeda.value].toFixed(2)} ${segundaMoeda.value}`
-
+  converterValor.textContent = (
+    valorInputAtualizado.value * moedaValorAtualizado
+  ).toFixed(2)
+  moedaConvertida.textContent = `1 ${segundaMoeda.value} = ${
+    1 * mudancaValorInput.conversion_rates[segundaMoeda.value].toFixed(2)
+  } ${segundaMoeda.value}`
 })
 
+primeiraMoeda.addEventListener('input', async event => {
+  mudancaValorInput = {
+    ...(await fetchBuscaTaxaCambio(getURL(event.target.value)))
+  }
 
+  converterValor.textContent = (
+    valorInputAtualizado.value *
+    mudancaValorInput.conversion_rates[segundaMoeda.value]
+  ).toFixed(2)
+
+  moedaConvertida.textContent = `1${primeiraMoeda.value} = ${1* mudancaValorInput.conversion_rates[segundaMoeda.value]} ${segundaMoeda.value}`
+})
 
 init()
